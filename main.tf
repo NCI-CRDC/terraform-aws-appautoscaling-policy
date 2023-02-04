31a3e83e9bea3aa4a3961da971aef6c8fe4cb863
Cole DeVries <colemandevries@gmail.com>
2023-02-04 15:42:03 -0500

    updated the outputs for autoscaling policy

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_appautoscaling_policy" "this" {
   name               = "${local.stack}-${var.resource_name_suffix}"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.microservice_autoscaling_target[each.key].resource_id
-  scalable_dimension = aws_appautoscaling_target.microservice_autoscaling_target[each.key].scalable_dimension
-  service_namespace  = aws_appautoscaling_target.microservice_autoscaling_target[each.key].service_namespace
+  policy_type        = var.policy_type
+  resource_id        = var.resouce_id
+  scalable_dimension = var.scalable_dimension
+  service_namespace  = var.service_namespace
 
   target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
+    target_value = var.target_value
 
-    target_value = 80
+    predefined_metric_specification {
+      predefined_metric_type = var.predefined_metric_type
+    }
   }
 }
